b8da0beddc1d60840967f97fcad0bc36a04871f0
Nghiath <thnghia2015@gmail.com>
2025-05-05 10:07:34 +0700

    Migrate table Customer into DB

---
+++ BookingRoom/Models/Customer.cs
@@ -0,0 +1,16 @@
+﻿namespace BookingRoom.Models
+{
+    public class Customer
+    {
+        public Guid Id { get; set; } = Guid.NewGuid();
+        public string FullName { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+        public string Phone { get; set; } = string.Empty;
+        public string Address { get; set; } = string.Empty;
+        public string IdentityNumber { get; set; } = string.Empty;
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+
+        // Navigation
+        public ICollection<Booking> Bookings { get; set; }
+    }
+}
